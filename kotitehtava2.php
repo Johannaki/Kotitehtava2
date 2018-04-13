eff26bdab2af82dc5a81fb46be65b0844e7353c0
Johanna Kinnari <johanna.kinnari@metropolia.fi>
2018-04-13 10:49:24 +0300

    Aloitus

---
+++ kotitehtava2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang="en">
+   <head>
+     <meta charset="UTF-8">
+     <title>Kotitehtävä2</title>
+     <style>
+     body {background-color: #fff3ef; padding: 40px;}
+     h2   {color: #5e5940;}
+     p    {color: #17183b;}
+     input {color: #17183b;}
+     </style>
+   </head>
+   <body>
+     <h2>Tehtävä 1</h2>
+     <form action="kotitehtava2.php" method="post">
+       <p>Montako tähteä haluat nähdä?:</p>
+       <input type="number" name="numeroni" value="" min="1" autofocus><br>
+       <input type="submit" value="Arvaa"><br><br>
+     </form>
+
+     <?php
+
+
+     ?>
+   </body>
+</html>
